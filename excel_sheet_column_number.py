7debd51a95910a2b8a3e16f84032b36dc1805c96
AntonChernichenko <anton.chernichenko@dohop.com>
2024-12-21 17:12:42 +0100

    title_to_number

---
+++ excel_sheet_column_number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        import string
+
+        m: dict[str, int] = {
+            e: i for i, e in enumerate(string.ascii_uppercase, start=1)
+        }
+
+        if len(columnTitle) == 1:
+            return m[columnTitle]
+
+        return sum(len(m) ** i * m[ch] for i, ch in enumerate(columnTitle[::-1]))
+
+
+assert Solution().titleToNumber('A') == 1
+assert Solution().titleToNumber('AB') == 28
+assert Solution().titleToNumber("ZY") == 701
