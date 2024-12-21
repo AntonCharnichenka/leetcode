a495f6c0fa973b04c400ce6ebf2abe1e7f59bcf6
AntonChernichenko <anton.chernichenko@dohop.com>
2024-12-21 17:26:46 +0100

    valid_palindrome

---
+++ valid_palindrome.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/valid-palindrome/description/
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered_s = ''.join(ch.lower() for ch in s if ch.isalnum())
+
+        return filtered_s == filtered_s[::-1]
+
+
+assert Solution().isPalindrome('A man, a plan, a canal: Panama')
+assert not Solution().isPalindrome('race a car')
+assert Solution().isPalindrome(' ')
