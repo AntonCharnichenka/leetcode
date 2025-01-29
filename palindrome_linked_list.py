cf3562421e37da79e234b9ec7e4b96a5a1b6fe73
AntonChernichenko <anton.chernichenko@dohop.com>
2025-01-29 08:14:28 +0100

    palindrom_linked_list

---
+++ palindrome_linked_list.py
@@ -0,0 +1,22 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: ListNode | None) -> bool:
+        values = [head.val]
+
+        while head.next:
+            head = head.next
+            values.append(head.val)
+
+        return values == values[::-1]
+
+
+assert (
+    Solution().isPalindrome(ListNode(1, ListNode(2, ListNode(2, ListNode(1, None)))))
+    is True
+)
+assert Solution().isPalindrome(ListNode(1, ListNode(2, None))) is False
